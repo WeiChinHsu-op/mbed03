884acffe069e72fcce3417d2e30424c87afe0fa0
WeiChinHsu-op <eric87092611@gmail.com>
2020-05-20 16:49:05 +0800

    a

--- 3_4_FFT_analysis/main.cpp
+++ 3_4_FFT_analysis/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
 Serial pc( USBTX, USBRX );
-AnalogOut Aout(DAC0_OUT);
+//AnalogOut Aout(DAC0_OUT);
 AnalogIn Ain(A0);
 
 int sample = 128;
@@ -11,12 +11,13 @@ float ADCdata[128];
 
 int main(){
   for (i = 0; i < sample; i++){
-    Aout = Ain;
+    //Aout = Ain;
     ADCdata[i] = Ain;
     wait(1./sample);
   }
   for (i = 0; i < sample; i++){
     pc.printf("%1.3f\r\n", ADCdata[i]);
-    wait(0.1);
+    wait(0.2);
   }
 }
+
